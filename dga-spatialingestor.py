3c1fa908ffe27c1ef8b8175b1d92b702689cf0fb
Alex Sadleir <alex.sadleir@linkdigital.com.au>
2014-08-22 03:03:00 +0000

    Fixes

--- dga-spatialingestor.py
+++ dga-spatialingestor.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # coding=utf-8
 '''
-spatial ingestor for ckan
+spatial ingestor for data.gov.au
 <alex.sadleir@linkdigital.com.au>
 1.0  28/11/2013  initial implementation
 1.1  25/03/2014  new create_resource technique for CKAN editing
 '''
-import ckanapi #https://github.com/ckan/ckanapi
+import ckanapi #https://github.com/open-data/ckanapi
 import errno, os, shutil, sys, glob
 from pprint import pprint 
 from email.mime.text import MIMEText
@@ -26,13 +26,13 @@ import lxml.etree as et
 geoserver_addr = "http://localhost:8080/geoserver/"
 geoserver_user = "admin"
 geoserver_passwd = ""
-email_addr = "admin@example.com"
+email_addr = "alex.sadleir@linkdigital.com.au, data.gov@finance.gov.au"
 shp2pgsql = "/usr/bin/shp2pgsql"
-omitted_orgs = []
+omitted_orgs = ['launcestoncitycouncil','gcc']
 
 def email(subject, body):
 	msg = MIMEText(body)
-	msg["From"] = ""
+	msg["From"] = "datagovau@gmail.com"
 	msg["To"] = email_addr
 	msg["Subject"] = subject
 	# Send the message via our own SMTP server, but don't include the
@@ -43,7 +43,7 @@ def email(subject, body):
 	s.ehlo()
 	s.starttls()
 	s.ehlo
-	s.login('example@gmail.com','')
+	s.login('datagovau@gmail.com','')
 	s.sendmail(msg["From"], [msg["To"]], msg.as_string())
 	s.quit()
 
@@ -71,28 +71,15 @@ def get_cursor(db_settings):
 	#cur.execute("create extension postgis")
 	return (cur,conn)
 
-def psql_load(proc):
-        (cur,conn) = get_cursor(db_settings)
-	sql = ""
-	for line in iter(proc.stdout.readline,''):
-	    sql += line
-            if sql.endswith(';'):
-		cur.execute(sql)
-		sql = ""
-	if sql != "":
-		#print sql
-		cur.execute(sql)
-        cur.close()
-        conn.close()
-
-if len(sys.argv) != 5:
-	print "spatial ingester. command line: postgis_url api_url api_key dataset_id"
+if len(sys.argv) != 6:
+	print "spatial ingester. command line: postgis_url api_url api_key geoserver_passwd dataset_id"
 	sys.exit(errno.EACCES)
 else:
-	(path, db_settings_json, api_url, api_key, dataset_id) = sys.argv
+	(path, db_settings_json, api_url, api_key, geoserver_passwd, dataset_id) = sys.argv
 	db_settings = json.loads(db_settings_json)
 
 ckan = ckanapi.RemoteCKAN(address=api_url, apikey=api_key)
+print dataset_id
 dataset = ckan.action.package_show(id=dataset_id)
 print "loaded dataset"+dataset['name']
 #pprint(dataset)
@@ -106,16 +93,19 @@ shp_resources = []
 data_modified_date = None
 for resource in dataset['resources']:
 	if "wms" in resource['format'] or "wfs" in resource['format']:
-		if geoserver_addr not in resource['url'] :
-			failure(dataset['id']+" already has geo api");
+		if 'geoserver' not in resource['url'] :
+			print(dataset['id']+" already has geo api");
+			sys.exit(0);
 		else:
 			ows_resources += [resource]
 		
-	if "kml" in resource['format'] or "kmz" in resource['format']:
-		data_modified_date = resource['last_modified']
+	if ("kml" in resource['format'] or "kmz" in resource['format']) and 'geoserver' not in resource['url']:
+		data_modified_date = resource['revision_timestamp']
+		print resource
 		kml_resources += [resource]
-	if "shp" in resource['format']:
-		data_modified_date = resource['last_modified']
+	if "shp" in resource['format'] and 'geoserver' not in resource['url']:
+		data_modified_date = resource['revision_timestamp']
+		print resource
 		shp_resources += [resource]
 
 if len(shp_resources) + len(kml_resources) == 0:
@@ -123,12 +113,15 @@ if len(shp_resources) + len(kml_resources) == 0:
 	sys.exit(0);
 
 #if geoserver api link does not exist or api link is out of date with data, continue
-if len(ows_resources) > 0 and parser.parse(data_modified_date) <= wms_resources[0]['last_modified']:
-	print "Already up to date"
-        sys.exit(0);
+if len(ows_resources) > 0:
+	print "Data modified: " + str(parser.parse(data_modified_date))
+	print "Geoserver last updated: " + str(parser.parse(ows_resources[0]['last_modified']))
+        if parser.parse(data_modified_date).date()  <= parser.parse(ows_resources[0]['last_modified']).date() :
+	    print "Already up to date"
+            sys.exit(0)
 
-email("geodata processing started for "+dataset['id'], "")
-msg = dataset['id']
+email("geodata processing started for "+dataset['title'], "Data modified: " + str(parser.parse(data_modified_date)) + "  Geoserver last updated: " + str(parser.parse(ows_resources[0]['last_modified'])))
+msg = dataset['title'] + "\n" + "https://data.gov.au/api/action/package_show?id="+dataset['id'] + "\n" + "https://data.gov.au/dataset/"+dataset['name']
 #download resource to tmpfile
 
 #check filesize limit
@@ -145,7 +138,7 @@ print tempdir+" created"
 #load esri shapefiles
 if len(shp_resources) > 0:# and False:
 	print "using SHP file "+shp_resources[0]['url']
-	(filepath,headers) = urllib.urlretrieve(shp_resources[0]['url'], "input.zip" )
+	(filepath,headers) = urllib.urlretrieve(shp_resources[0]['url'].replace('https','http'), "input.zip" )
 	print "shp downlaoded"
 	with ZipFile(filepath, 'r') as myzip:
 		myzip.extractall()
@@ -155,16 +148,21 @@ if len(shp_resources) > 0:# and False:
 	if len(shpfiles) == 0:
 		failure("no shp files found in zip "+shp_resources[0]['url'])
 	print "converting to pgsql "+table_name+" "+shpfiles[0]
-	process = Popen([shp2pgsql,shpfiles[0], table_name], stdout=PIPE, stderr=PIPE)
-	psql_load(process)
+	pargs = ['ogr2ogr','-f','PostgreSQL',"--config" ,"PG_USE_COPY","YES",'PG:dbname=\''+ db_settings['dbname']+'\' host=\''+db_settings['host']+'\' user=\''+db_settings['user']+ '\' password=\''+db_settings['password']+'\'' 
+		,tempdir,'-lco','GEOMETRY_NAME=geom',"-lco", "PRECISION=NO",'-nln',table_name,'-a_srs', 'EPSG:4326','-nlt','PROMOTE_TO_MULTI'] #'MULTIPOLYGON'] # TODO, change to PROMOTE_TO_MULTI http://lists.osgeo.org/pipermail/gdal-dev/2012-September/034128.html
+	pprint(pargs)
+	p = Popen(pargs)#, stdout=PIPE, stderr=PIPE)
+	p.communicate()
 	if len(prjfiles) > 0:
 		nativeCRS = open(prjfiles[0], 'r').read()
+	else:
+		nativeCRS = None
 else:
 	print "using KML file "+kml_resources[0]['url']
 	nativeCRS = None
 	#if kml ogr2ogr http://gis.stackexchange.com/questions/33102/how-to-import-kml-file-with-custom-data-to-postgres-postgis-database
 	if kml_resources[0]['format'] == "kmz":
-		(filepath,headers) = urllib.urlretrieve(kml_resources[0]['url'], "input.zip" )
+		(filepath,headers) = urllib.urlretrieve(kml_resources[0]['url'].replace('https','http'), "input.zip" )
 		with ZipFile(filepath, 'r') as myzip:
 			myzip.extractall()
 		print "kmz unziped"
@@ -174,7 +172,7 @@ else:
 		else:
 			kml_file = kmlfiles[0]
 	else: 
-		(filepath,headers) = urllib.urlretrieve(kml_resources[0]['url'], "input.kml")
+		(filepath,headers) = urllib.urlretrieve(kml_resources[0]['url'].replace('https','http'), "input.kml")
 		kml_file = "input.kml"
 	print "changing kml folder name"
 	tree = et.parse(kml_file)
@@ -183,7 +181,9 @@ else:
 	with open(table_name+".kml", "w") as ofile: 
 		ofile.write(et.tostring(tree))
 	print "converting to pgsql "+table_name+".kml"
-	pargs = ['ogr2ogr','-f','PostgreSQL',"--config" ,"PG_USE_COPY","YES",'PG:dbname=\''+ db_settings['dbname']+'\' host=\''+db_settings['host']+'\' user=\''+db_settings['user']+ '\' password=\''+db_settings['password']+'\'' ,table_name+".kml",'-lco','GEOMETRY_NAME=geom']
+	pargs = ['ogr2ogr','-f','PostgreSQL',"--config" ,"PG_USE_COPY","YES",
+		'PG:dbname=\''+ db_settings['dbname']+'\' host=\''+db_settings['host']+'\' user=\''+db_settings['user']+ '\' password=\''+db_settings['password']+'\'' 
+		,table_name+".kml",'-lco','GEOMETRY_NAME=geom']
 	pprint(pargs)
 	p = Popen(pargs)#, stdout=PIPE, stderr=PIPE)
 	p.communicate()
@@ -215,7 +215,8 @@ dsdata =json.dumps({'dataStore':{'name':datastore,
      'user':db_settings['user'] + "_data", #use read only user
      'passwd':db_settings['password'],
      'dbtype':'postgis'
-
+     "validate connections": "true",
+     "Support on the fly geometry simplification":"true",
                 }}})
 print dsdata
 r = requests.post(geoserver_addr+'rest/workspaces/'+workspace+'/datastores', data=dsdata, headers={'Content-type': 'application/json'}, auth=(geoserver_user, geoserver_passwd))
@@ -228,7 +229,7 @@ ftdata = {'featureType':{'name':table_name, 'title': dataset['title']}}
 (minx,miny, maxx, maxy) = bbox.replace("BOX","").replace("(","").replace(")","").replace(","," ").split(" ")
 bbox_obj = { 'minx': minx,'maxx': maxx,'miny': miny,'maxy': maxy }
 
-if nativeCRS != None:
+if nativeCRS and nativeCRS != None:
 	ftdata['featureType']['nativeCRS'] = nativeCRS
 else:
 	ftdata['featureType']['nativeBoundingBox'] = bbox_obj
@@ -239,7 +240,6 @@ print geoserver_addr+'rest/workspaces/'+workspace+'/datastores/'+datastore+"/fea
 print ftdata
 r = requests.post(geoserver_addr+'rest/workspaces/'+workspace+'/datastores/'+datastore+"/featuretypes", data= ftdata, headers={'Content-Type': 'application/json'}, auth=(geoserver_user, geoserver_passwd))
 pprint(r)
-
 #generate wms/wfs api links, kml, png resources and add to package
 print bgjson
 dataset['spatial'] = bgjson
@@ -258,11 +258,14 @@ for format in ['csv','json']:
        url = ws_addr+"wfs?request=GetFeature&typeName="+table_name+"&outputFormat="+urllib.quote(format)
        #if format == "csv" and format not in existing_formats:
        #        ckan.call_action('resource_create',{"package_id":dataset['id'],"name": dataset['title'] + " CSV","description":"For summary of the objects/data in this collection","format":format,"url":url, "last_modified": datetime.now().isoformat()})
-       if format == "json" and format not in existing_formats:
+       if format == "json" and format not in existing_formats and "JSON" not in existing_formats:
                ckan.call_action('resource_create',{"package_id":dataset['id'],"name":dataset['title'] + " GeoJSON","description":"For use in web-based data visualisation of this collection","format":format,"url":url, "last_modified": datetime.now().isoformat()})
 if "wms" not in existing_formats:
 	ckan.call_action('resource_create',{"package_id":dataset['id'],"name":dataset['title'] + " - Preview this Dataset (WMS)","description":"View the data in this dataset online via an online map","format":"wms",
 	    "url":ws_addr+"wms?request=GetCapabilities", "last_modified": datetime.now().isoformat()})
+else:
+    for ows in ows_resources:
+        ckan.call_action('resource_update',ows)
 if "wfs" not in existing_formats:
 	ckan.call_action('resource_create',{"package_id":dataset['id'],"name":dataset['title'] + " Web Feature Service API Link","description":"WFS API Link for use in Desktop GIS tools","format":"wfs",
 	    "url":ws_addr+"wfs?request=GetCapabilities", "last_modified": datetime.now().isoformat()})
